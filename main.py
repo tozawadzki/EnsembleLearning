b1f459895d028cdc6267beca5e5bd0380ce5215b
ARRIBATEC\Tza0148 <Tza0148@arribatec.com>
2022-01-12 20:40:48 +0100

    pseudoplot

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from sklearn.neural_network import MLPClassifier
 #Do wczytywania danych z excela
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 #Dane
 from sklearn.metrics import accuracy_score
@@ -37,11 +38,11 @@ X_train, X_test, Y_train, Y_test = train_test_split(X,
 def ensemble_voting():
     estimator = []
 
-    estimator.append(('DTC', DecisionTreeClassifier(random_state=0)))
+    estimator.append(('DTC', DecisionTreeClassifier(min_samples_split=5, min_samples_leaf=3, random_state=0)))
+    estimator.append(('RF', MLPClassifier(hidden_layer_sizes=(10), max_iter=10000, random_state=0)))
     estimator.append(('KNN', KNeighborsClassifier(n_neighbors=3)))
     estimator.append(('SVC', SVC(gamma='auto', probability=True)))
     estimator.append(('LR', LogisticRegression(random_state=10, max_iter=1000)))
-    estimator.append(('RF', MLPClassifier(hidden_layer_sizes=(10), max_iter=10000, random_state=0)))
 
     ensemble_voting_soft = VotingClassifier(estimators = estimator, voting='soft')
     ensemble_voting_soft.fit(X_train, Y_train)
@@ -56,9 +57,20 @@ def ensemble_voting():
     hard_accuracy = accuracy_score(Y_test, hard_predict)
 
     print("Prediction")
-    print("Soft voting: ", soft_predict, "\nHard voting: ", hard_predict)
-    print("Data set:    ", Y_test)
+    print("Data set:   ", Y_test)
+    print("Soft voting:", soft_predict, "\nHard voting:", hard_predict)
     print("Accuracy")
-    print("Soft voting: ", soft_accuracy, "\nHard voting: ", hard_accuracy)
+    print("Soft voting:", soft_accuracy, "\nHard voting:", hard_accuracy)
+
+    plt.figure(figsize=(5,5))
+    plt.scatter(Y_test, soft_predict, c='crimson')
+
+    p1 = max(max(soft_predict), max(Y_test))
+    p2 = min(min(soft_predict), min(Y_test))
+    plt.plot([p1, p2], [p1, p2], 'b-')
+    plt.xlabel('True Values', fontsize=10)
+    plt.ylabel('Predictions', fontsize=10)
+    plt.axis('equal')
+    plt.show()
 
 ensemble_voting()
