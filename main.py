2f9eb9aeb12dd78aba62f07828ba54bd5c4270e0
Your Name <you@example.com>
2022-01-19 00:28:34 +0100

    cls permutation

--- main.py
+++ main.py
@@ -14,31 +14,31 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 dataSetsNames = [
-    'data1',
-    'data2',
-    'data3',
-    'data4',
-    'data5',
-    'data6',
-    'data7',
-    'data8',
-    'data9',
-    'data10',
-    'data11',
-    'data12',
-    'data13',
-    'data14',
-    'data15',
-    'data16',
-    'data17',
-    'data18',
-    'data19',
-    'data20',
+    'data1'#,
+    #'data2',
+    #'data3',
+    #'data4',
+    #'data5',
+    #'data6',
+    #'data7',
+    #'data8',
+    #'data9',
+    #'data10',
+    #'data11',
+    #'data12',
+    #'data13',
+    #'data14',
+    #'data15',
+    #'data16',
+    #'data17',
+    #'data18',
+    #'data19',
+    #'data20',
 ]
 
 
-def writeToFile(soft_accuracy, hard_accuracy):
-    f = open("results.txt", "a")
+def writeToFile(soft_accuracy, hard_accuracy, y):
+    f = open("results_base_classifiers{}.txt".format(y+1), "a")
     f.write(x)
     f.write("\n")
     f.write("Soft voting: ")
@@ -64,46 +64,63 @@ def createPlot(soft_predict, Y_test):
 
 
 def ensemble_voting(x):
+
+    base_classifiers1 = []
+    base_classifiers2 = []
+    
+    # base_classifiers1 appendings
+    base_classifiers1.append(('KNN', KNeighborsClassifier(algorithm='auto')))
+    base_classifiers1.append(('DTC', DecisionTreeClassifier()))
+    base_classifiers1.append(('LR', LogisticRegression(max_iter=1000000)))
+    base_classifiers1.append(('GNB', GaussianNB()))
+    base_classifiers1.append(('SVC', SVC(gamma="auto", probability=True)))
+
+    # base_classifiers2 appendings
+    base_classifiers2.append(('KNN', KNeighborsClassifier(algorithm='auto')))
+    base_classifiers2.append(('DTC', DecisionTreeClassifier()))
+    base_classifiers2.append(('LR', LogisticRegression(max_iter=1000000)))
+    base_classifiers2.append(('GNB', GaussianNB()))
+    base_classifiers2.append(('SVC', SVC(gamma="auto", probability=True)))
+
+
     base_classifiers = []
+    base_classifiers.append(base_classifiers1)
+    base_classifiers.append(base_classifiers2)
 
-    base_classifiers.append(('KNN', KNeighborsClassifier()))
-    base_classifiers.append(('DTC', DecisionTreeClassifier()))
-    base_classifiers.append(('LR', LogisticRegression(max_iter=1000000)))
-    base_classifiers.append(('GNB', GaussianNB()))
-    base_classifiers.append(('SVC', SVC(gamma="auto", probability=True)))
+    for y in range(0,len(base_classifiers)):
 
-    ensemble_voting_soft = VotingClassifier(
-        estimators=base_classifiers, voting='soft')
-    ensemble_voting_soft.fit(X_train, Y_train)
+        currentClassifiers = base_classifiers[y]
 
-    ensemble_voting_hard = VotingClassifier(
-        estimators=base_classifiers, voting='hard')
-    ensemble_voting_hard.fit(X_train, Y_train)
+        ensemble_voting_soft = VotingClassifier(
+            estimators=currentClassifiers, voting='soft')
+        ensemble_voting_soft.fit(X_train, Y_train)
 
-    soft_predict = ensemble_voting_soft.predict(X_test)
-    hard_predict = ensemble_voting_hard.predict(X_test)
+        ensemble_voting_hard = VotingClassifier(
+            estimators=currentClassifiers, voting='hard')
+        ensemble_voting_hard.fit(X_train, Y_train)
 
-    soft_accuracy = accuracy_score(Y_test, soft_predict)
-    hard_accuracy = accuracy_score(Y_test, hard_predict)
+        soft_predict = ensemble_voting_soft.predict(X_test)
+        hard_predict = ensemble_voting_hard.predict(X_test)
 
-    print("Prediction")
-    print("Data set:   ", Y_test)
-    print("Soft voting:", soft_predict, "\nHard voting:", hard_predict)
-    print("Accuracy")
-    print("Soft voting:", soft_accuracy, "\nHard voting:", hard_accuracy)
+        soft_accuracy = accuracy_score(Y_test, soft_predict)
+        hard_accuracy = accuracy_score(Y_test, hard_predict)
 
-    writeToFile(soft_accuracy, hard_accuracy)
-    #createPlot(soft_predict, Y_test)
+        print("Prediction")
+        print("Data set:   ", Y_test)
+        print("Soft voting:", soft_predict, "\nHard voting:", hard_predict)
+        print("Accuracy")
+        print("Soft voting:", soft_accuracy, "\nHard voting:", hard_accuracy)
+
+        writeToFile(soft_accuracy, hard_accuracy, y)
+        #createPlot(soft_predict, Y_test)
 
 
 for x in dataSetsNames:
 
-    df = pd.read_excel("data\{}.xls".format(x), sheet_name="Sheet1")
+    df = pd.read_excel("{}.xls".format(x), sheet_name="Sheet1")
     X = np.array(df, dtype='float32')
-    Y = np.array(pd.read_excel("data\{}.xls".format(x), sheet_name="Sheet2"))
+    Y = np.array(pd.read_excel("{}.xls".format(x), sheet_name="Sheet2"))
     Y = Y.ravel()
-
-    print(Y.size)
 
     X_train, X_test, Y_train, Y_test = train_test_split(X,
                                                         Y,
